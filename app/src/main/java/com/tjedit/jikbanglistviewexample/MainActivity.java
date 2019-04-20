16d58f3a57ca38b21678ec66c3ede80c048c094a
user <kyronko@naver.com>
2019-04-20 15:51:28 +0900

    리스트뷰와 어댑터를 메인 액티비티에서 연결.

--- app/src/main/java/com/tjedit/jikbanglistviewexample/MainActivity.java
+++ app/src/main/java/com/tjedit/jikbanglistviewexample/MainActivity.java
@@ -4,6 +4,7 @@ import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.tjedit.jikbanglistviewexample.adapters.RoomAdapter;
 import com.tjedit.jikbanglistviewexample.databinding.ActivityMainBinding;
 import com.tjedit.jikbanglistviewexample.datas.Room;
 
@@ -11,25 +12,42 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    List<Room> roomList =new ArrayList<>();
+
+    RoomAdapter mRoomAdapter;
+
+    List<Room> roomList = new ArrayList<>();
     ActivityMainBinding act;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        act = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        act = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
         act.cleanRoomCounTxt.setText("32개");
 
         fillRooms();
 
+        mRoomAdapter = new RoomAdapter(MainActivity.this, roomList);
+        act.roomListView.setAdapter(mRoomAdapter);
 
 
     }
-    void fillRooms(){
-        roomList.add(new Room( 500, 30, "서구 둔산동", 7, "타임월드 인근"));
-        roomList.add(new Room( 500, 40, "북구 둔산동", 3, "갤러리 인근"));
-        roomList.add(new Room( 500, 35, "남구 둔산동", 10, "타드 인근"));
-        roomList.add(new Room( 500, 40, "동구 둔산동",  15, "월드 인근"));
-        roomList.add(new Room( 500, 35, "북구 둔산동", 15, "타임월드 인근"));
+
+    void fillRooms() {
+        roomList.add(new Room(500, 30, "서구 둔산동", 7, "타임월드 인근"));
+        roomList.add(new Room(500, 40, "북구 둔산동", 3, "갤러리 인근"));
+        roomList.add(new Room(500, 35, "남구 둔산동", 10, "타드 인근"));
+        roomList.add(new Room(500, 40, "동구 둔산동", 15, "월드 인근"));
+        roomList.add(new Room(500, 35, "북구 둔산동", 15, "타임월드 인근"));
+        roomList.add(new Room(500, 30, "서구 둔산동", 7, "타임월드 인근"));
+        roomList.add(new Room(500, 40, "북구 둔산동", 3, "갤러리 인근"));
+        roomList.add(new Room(500, 35, "남구 둔산동", 10, "타드 인근"));
+        roomList.add(new Room(500, 40, "동구 둔산동", 15, "월드 인근"));
+        roomList.add(new Room(500, 35, "북구 둔산동", 15, "타임월드 인근"));
+        roomList.add(new Room(500, 30, "서구 둔산동", 7, "타임월드 인근"));
+        roomList.add(new Room(500, 40, "북구 둔산동", 3, "갤러리 인근"));
+        roomList.add(new Room(500, 35, "남구 둔산동", 10, "타드 인근"));
+        roomList.add(new Room(500, 40, "동구 둔산동", 15, "월드 인근"));
+        roomList.add(new Room(500, 35, "북구 둔산동", 15, "타임월드 인근"));
     }
 }
